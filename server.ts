fa5e6efac86c1f99e47061da1312639848d7c423
Kushal <dev@kushalkumarsaha.com>
2024-02-23 18:21:07 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -12,17 +12,29 @@ const PORT = process.env.PORT || 5001
 
 // @GET
 app.get('/', async (req,res) => {
+    // GREET USER ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    if(Object.keys(req.query).length===0){
+        return res.status(200).json({
+            "message": "Welcome to IconZ!",
+            "instructions": "To get started, send a GET request to / with your desired brand name as a query parameter.",
+            "example_endpoint": "/?gatsby=svg&scss=all&sqlite=png",
+            "supported_formats": [ "svg", "png" ]
+        })
+    }
+
+    // HANDLE REAL REQUEST ++++++++++++++++++++++++++++++++++++++++++++++++++++++
     let result = {}
 
     const params = req.query
     let keys = Object.keys(params)
-    console.log(params)
     
-    const MAX_REQUEST_LENGTH = Math.min(20,keys.length)
+    const MAX_REQUEST_LENGTH = Math.min(20,keys.length)  // total of 20 requests per bandwidth
 
     for(let i=0; i<MAX_REQUEST_LENGTH;i++){
         const key = keys[i]
-        const data = await selectJSONFile(key)
+        // GET DATA ---------------------------
+        const data = await selectJSONFile(key) 
+        // FILTER DATA ------------------------
         if(Object.keys(data).length!==0){
             if(params[key]==='png')                             result[key] = { png: data.png }
             else if(params[key]==='svg')                        result[key] = { svg: data.svg } 
