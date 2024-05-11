76a76900af4f3e3642ee05b3f042a34394f49057
Kushal <dev@kushalkumarsaha.com>
2024-05-11 08:34:45 +0530

    Add files via upload

--- server.ts
+++ server.ts
@@ -1,52 +1,106 @@
 import express, { json } from 'express'
 import cors from 'cors'
-import { selectJSONFile } from './jsonNavigator'
+import { selectJSONFile } from './helpers/selectJSONFile'
+import { PORT as DEFAULT_PORT } from './constants/constants'
+import { API_ROUTE, MAX_REQUEST_LENGTH } from './constants/constants'
+import { MSGS } from './constants/messages'
+import { showSupportedList } from './helpers/showSupportedList'
+import { rootData, typesList } from './helpers/readJSON'
+
+
+// =========================================================================================================
+// =====[ INITIALIZATIONS ]======================================================================================
+// =========================================================================================================
 
 const app = express()
 app.use(cors())
 app.use(json())
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || DEFAULT_PORT
 
-// ==================================================================================
-// ==================================================================================
 
-// @GET
-app.get('/', async (req,res) => {
-    // GREET USER ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+// =========================================================================================================
+// =====[ ENDPOINTS ]======================================================================================
+// =========================================================================================================
+
+// PUBLIC USE ENDPOINT +++++++++++++++++++++++++
+app.get(API_ROUTE.PUBLIC_USE_ENDPOINT, async (req,res) => {
+
+    // welcome message -------------------------
     if(Object.keys(req.query).length===0){
-        return res.status(200).json({
-            "message": "Welcome to IconZ!",
-            "instructions": "To get started, send a GET request to / with your desired brand name as a query parameter.",
-            "example_endpoint": "/?gatsby=svg&scss=all&sqlite=png",
-            "supported_formats": [ "svg", "png" ]
-        })
+        return res.status(200).json(MSGS.WELCOME_MSG)
     }
 
-    // HANDLE REAL REQUEST ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    let result = {}
+
+    // return requested data -------------------
+    let result: { [key:string]: { png?:string, svg?:string } } = {}
 
     const params = req.query
     let keys = Object.keys(params)
     
-    const MAX_REQUEST_LENGTH = Math.min(20,keys.length)  // total of 20 requests per bandwidth
+    const requestLength = Math.min(MAX_REQUEST_LENGTH,keys.length)
 
-    for(let i=0; i<MAX_REQUEST_LENGTH;i++){
-        const key = keys[i]
-        // GET DATA ---------------------------
-        const data = await selectJSONFile(key) 
-        // FILTER DATA ------------------------
-        if(Object.keys(data).length!==0){
-            if(params[key]==='png')                             result[key] = { png: data.png }
-            else if(params[key]==='svg')                        result[key] = { svg: data.svg } 
-            else if(params[key]==='' || params[key]==='all')    result[key] = data
+    for(let i=0; i<requestLength;i++){
+        const brandName = keys[i]
+        const data = await selectJSONFile(brandName)
+
+        const keyList = Object.keys(data)
+        if(keyList.length!==0 && keyList[0]!==""){
+            if(params[brandName]==='png')                                   result[brandName] = { png: data.png }
+            else if(params[brandName]==='svg')                              result[brandName] = { svg: data.svg } 
+            else if(params[brandName]==='' || params[brandName]==='all')    result[brandName] = data
             else{}
         }
     }
 
-    return res.status(200).json({ success:true, data: result })
+    if(Object.keys(result).length > 0)
+        return res.status(200).json({ success:true, data: result })
+    
+    return res.status(200).json({ success:true, data: {}, error: MSGS.ERROR_MSG })
 })
 
-// ==================================================================================
-// ==================================================================================
 
-app.listen( PORT, () => console.log(`Iconz listening at port ${PORT}...`))
+
+
+// SHOW SUPPORTED NAME LIST +++++++++++++++++++++++++
+app.get(API_ROUTE.ALL_SUPPORTED_LIST, async (req,res) => {
+    const msg = MSGS.SUPPORTED_FULL_LIST_MSG
+    const data = await showSupportedList(rootData, typesList)
+    return res.status(200).json({ success:true, message:msg, data:data })
+})
+
+
+
+for(let i=0; i<typesList.length; i++) {
+    const typeName = typesList[i]
+    const endpoint = `${API_ROUTE.ALL_SUPPORTED_LIST}/${typeName.toLowerCase()}`
+
+    // SHOW FULL SUPPORTED LIST FOR DISTINCT CATEGORY ++++++++++++++++++++++++++
+    app.get(endpoint, async (req,res) => {
+        const data = rootData[i]
+        const otherTypes = typesList.filter((type,index) => index!==i)
+                                    .map((type,index) => { 
+                                        return `${API_ROUTE.ALL_SUPPORTED_LIST}/${type}` 
+                                    })
+        return res.status(200).json({ success:true, data:{ type:typeName, list:data, other_supported_types:otherTypes } })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+// =========================================================================================================
+// =====[ SERVER LISTENER ]======================================================================================
+// =========================================================================================================
+
+app.listen( PORT, () => console.log(`iconZ listening at port ${PORT}...`))
+
+module.exports = app
